ad26ef67282f7112c372ae396be4d7d5a3c1a3ba
Gerald Kwok <841784596@qq.com>
2018-11-14 18:26:41 +0800

    [郭文峰][更新][2018/11/14] 修改原错误理解程序，strcmp是比较每个字符的ASCII，有不一样就返回了，而不是将所有的字符ASCII加起来比较

--- C语言/day 16/5. 模拟实现strcmp/5. 模拟实现strcmp/源.c
+++ C语言/day 16/5. 模拟实现strcmp/5. 模拟实现strcmp/源.c
@@ -14,29 +14,26 @@
 int my_strcmp(const char *s1, const char *s2)
 {
 	assert(s1 && s2);
-	unsigned int count1 = 0;
-	unsigned int count2 = 0;
 
-	do
+	while (*s1 == *s2 && *s1 && *s2)
 	{
-		count1 = *s1;
-		count2 = *s2;
-
-		//如果s1结束，有两种情况
-		//1 两个字符串相等，那么将会返回0
-		//2 s1小于s2，那么上面的赋值，count1将会得到一个0,而count2将会得到一个正值
-		//所以将会返回负数
-		if (*s1 == '\0')
-			return count1 - count2;
-
 		s1++;
 		s2++;
-	} while (count1 == count2);
+	}
 
-	//这里也是只存在两种情况
-	//1 count1小于count2
-	//2 count1大于count2
-	return count1 - count2;
+	if (*s1 == *s2)
+	{
+		return 0;
+	}
+	else if (*s1 > *s2)
+	{
+		return 1;
+	}
+	else
+	{
+		return -1;
+	}
+
 	
 }
 
