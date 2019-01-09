acf0b729415c4b09648f62b216c637743bb83953
Gerald Kwok <841784596@qq.com>
2019-01-09 11:50:32 +0800

    [郭文峰][更新][2019/1/9] 增加了几道OJ题

--- 数据结构/3_双向有头链表/3_双向有头链表/OJ.c
+++ 数据结构/3_双向有头链表/3_双向有头链表/OJ.c
@@ -92,7 +92,112 @@ ListNode *mergeOrderedList(ListNode *l1, ListNode *l2)
 	return new_head;
 }
 
+// 编写代码，以给定值x为基准将链表分割成两部分，所有小于x的结点排在大于或等于x的结点之前
+ListNode* partition(ListNode* pHead, int x) {
+	// write code here
+	if (pHead == NULL)
+		return NULL;
+
+	struct ListNode *curLT = pHead;
+	struct ListNode *curGT = pHead;
+	struct ListNode *cur = pHead;
+	struct ListNode *gTHead = NULL;
+	struct ListNode *new_head = NULL;
 
 
+	while (cur)
+	{
+		if (cur->val < x)
+		{
+			if (new_head == NULL)
+			{
+				new_head = cur;
+				curLT = cur;
+			}
+			else
+			{
+				curLT->next = cur;
+				curLT = cur;
+			}
+			cur = cur->next;
+		}
+		else
+		{
+			if (gTHead == NULL)
+			{
+				gTHead = cur;
+				curGT = cur;
+			}
+			else
+			{
+				curGT->next = cur;
+				curGT = cur;
+			}
+			cur = cur->next;
+		}
+	}
+	if (gTHead == NULL)
+		curLT->next = NULL;
+	else if (new_head == NULL)
+	{
+		new_head = gTHead;
+		curGT->next = NULL;
+	}
+	else
+	{
+		curGT->next = NULL;
+		curLT->next = gTHead;
+	}
+
+	return new_head;
+}
 
 
+//删除重复结点
+ListNode* deleteDuplication(ListNode* pHead)
+{
+	if (pHead == NULL) {
+		return NULL;
+	}
+
+	ListNode *prev = NULL;	// 用于删除的结点，是 p1 的前一个结点
+	ListNode *p1 = pHead;
+	ListNode *p2 = pHead->next;
+	ListNode *result = pHead;		// 用于返回的第一个结点
+
+	while (p2 != NULL) {
+		if (p1->val != p2->val) {
+			prev = p1;
+			p1 = p2;
+			p2 = p2->next;
+		}
+		else {
+			while (p2 != NULL && p2->val == p1->val) {
+				p2 = p2->next;
+			}
+
+			// 删除
+			ListNode *next;
+			for (ListNode *node = p1; node != p2; node = next) {
+				next = node->next;
+				free(node);
+			}
+
+			// 重新拼接链表
+			if (prev != NULL) {
+				prev->next = p2;
+			}
+			else {
+				// 1 --> 1 --> 1 --> 2 --> NULL
+				// 的情况
+				result = p2;
+			}
+
+			p1 = p2;
+			if (p2 != NULL) {
+				p2 = p2->next;
+			}
+		}
+	}
+	return result;
+}
